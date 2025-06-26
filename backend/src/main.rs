d56b86581b6dab5baa0b786f7d0a4a03d3d21034
Aditya Singh <jeevan56k@gmail.com>
2025-06-27 00:13:20 +0530

    Written a basic server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,2 +1,18 @@
 use axum::{routing::get,Router, Json};
-use std::net::socketAddr;
+use std::net::SocketAddr;
+use serde_json::json;
+use tokio;
+use axum::http::StatusCode;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/",get(root_handler));
+    let addr= SocketAddr::from(([127,0,0,1],3000));
+    println!("Server running on http://{}",addr);
+    axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(),app)
+        .await
+        .unwrap();
+}
+async fn root_handler() -> Json<serde_json::Value>{
+    Json(json!({"message": "Backend Working"}))
+}
