db9823a9e6110de255d367e4c1e55463c54c9dd5
Aditya Singh <jeevan56k@gmail.com>
2025-06-27 00:20:10 +0530

    Setting up download_handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,32 @@
-use axum::{routing::get,Router, Json};
+use axum::{extract::Query,response::IntoResponse,routing::get,Router, Json};
 use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tokio;
 use axum::http::StatusCode;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/",get(root_handler));
+    let app = Router::new()
+        .route("/",get(root_handler))
+        .route("/api/downlaod",get(download_handler));
+    
     let addr= SocketAddr::from(([127,0,0,1],3000));
     println!("Server running on http://{}",addr);
+    
     axum::serve(tokio::net::TcpListener::bind(addr).await.unwrap(),app)
         .await
         .unwrap();
 }
 async fn root_handler() -> Json<serde_json::Value>{
     Json(json!({"message": "Backend Working"}))
+}
+
+struct DownloadQuery{
+    url:String,
+}
+struct DownloadResponse{
+    success:bool.
+    media_url:Option<String>,
+    error:Option<String>,
 }
